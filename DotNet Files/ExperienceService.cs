1ff428d9f3575f3720a74aae375814fb8d44a25e
Casey Roy <caseyroy@gmail.com>
2023-05-24 13:46:32 -0700

    Added exp service and js service

---
+++ DotNet Files/ExperienceService.cs
@@ -0,0 +1,60 @@
+﻿using Sabio.Data;
+using Sabio.Data.Providers;
+using Sabio.Models.Domain;
+using Sabio.Models.Domain.JobSkills;
+using Sabio.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Services
+{
+    public class ExperienceService : IExperienceService
+    {
+        private ILookUpService _lookUpService;
+        private IDataProvider _dataProvider;
+
+        public ExperienceService(ILookUpService lookUpService, IDataProvider dataProvider)
+        {
+            _lookUpService = lookUpService;
+            _dataProvider = dataProvider;
+        }
+        public List<LookUp3Col> GetAllExperience()
+        {
+            List<LookUp3Col> skillsets = null;
+            string procName = "[dbo].[ExperienceLevels_SelectAll]";
+
+            _dataProvider.ExecuteCmd(procName, delegate (SqlParameterCollection col)
+            {
+            }, delegate (IDataReader reader, short set)
+            {
+                int index = 0;
+                LookUp3Col singleset = MapExperience(reader, ref index);
+
+                if (skillsets == null)
+                {
+                    skillsets = new List<LookUp3Col>();
+                }
+                skillsets.Add(singleset);
+            });
+
+            return skillsets;
+
+        }
+
+        private LookUp3Col MapExperience(IDataReader reader, ref int startingIndex)
+        {
+            LookUp3Col skillset = new LookUp3Col();
+
+            skillset.Id = reader.GetSafeInt32(startingIndex++);
+            skillset.Name = reader.GetSafeString(startingIndex++);
+            skillset.Col3 = reader.GetSafeString(startingIndex++);
+
+            return skillset;
+        }
+    }
+}
---
+++ DotNet Files/JobSkillsService.cs
@@ -0,0 +1,109 @@
+﻿using Sabio.Data.Providers;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using Sabio.Models.Requests;
+using Sabio.Models.Domain;
+using Sabio.Data;
+using System.Collections.Generic;
+using Sabio.Services.Interfaces;
+using Sabio.Models.Domain.JobSkills;
+
+namespace Sabio.Services
+{
+    public class JobSkillService : IJobSkillsService
+    {
+        private ILookUpService _lookUpService;
+        private IDataProvider _dataProvider;
+
+        public JobSkillService(ILookUpService lookUpService, IDataProvider dataProvider)
+        {
+            _lookUpService = lookUpService;
+            _dataProvider = dataProvider;
+        }
+
+        public void Create(JobSkillsAddRequest request)
+        {
+            string procName = "[dbo].[JobSkills_Insert]";
+            _dataProvider.ExecuteNonQuery(procName, inputParamMapper: delegate (SqlParameterCollection col)
+            {
+
+                AddCommonParams(request, col);
+
+
+            }, returnParameters: null);
+        }
+
+        public void Update(JobSkillsAddRequest request) 
+        {
+            string procName = "[dbo].[JobSkills_Update]";
+            _dataProvider.ExecuteNonQuery(procName, inputParamMapper: delegate (SqlParameterCollection col)
+            {
+                AddCommonParams(request, col);
+
+            }, returnParameters: null);
+        
+        }
+
+        public void Delete(int jobId, int skillId)
+        {
+            string procName = "[dbo].[JobSkills_Delete]";
+
+            _dataProvider.ExecuteNonQuery(procName, delegate (SqlParameterCollection col)
+            {
+                col.AddWithValue("@JobId", jobId);
+                col.AddWithValue("@SkillId", skillId);
+
+            });
+
+        }
+
+        public List<JobSkills> GetByJobId(int jobId)
+        {
+            List<JobSkills> skillsets = null;
+            string procName = "[dbo].[JobSkills_Select_ByJobId]";
+
+            _dataProvider.ExecuteCmd(procName, delegate (SqlParameterCollection col)
+            {
+                col.AddWithValue("@JobId", jobId);
+            }, delegate (IDataReader reader, short set)
+            {
+                int index = 0;
+                JobSkills singleset = MapSingleJobSkillsSet(reader, ref index);
+
+                if (skillsets == null)
+                {
+                    skillsets = new List<JobSkills>();
+                }
+                skillsets.Add(singleset);
+            });
+
+            return skillsets;
+
+        }
+
+    private  JobSkills MapSingleJobSkillsSet(IDataReader reader, ref int startingIndex)
+        {
+            JobSkills skillset = new JobSkills();
+
+            skillset.JobId = reader.GetSafeInt32(startingIndex++);
+            skillset.Skill = _lookUpService.MapSingleLookUp(reader, ref startingIndex);
+            skillset.Experience = _lookUpService.MapSingleLookUp(reader, ref startingIndex);
+            skillset.YearsStart = reader.GetSafeInt32(startingIndex++);
+            skillset.YearsEnd = reader.GetSafeInt32(startingIndex++);
+
+            return skillset;
+        }
+
+        private static void AddCommonParams(JobSkillsAddRequest request, SqlParameterCollection col)
+        {
+            col.AddWithValue("@JobId", request.JobId);
+            col.AddWithValue("@SkillId", request.SkillId);
+            col.AddWithValue("@ExperienceLevelId", request.ExperienceLevelId);
+            col.AddWithValue("@YearsRangeStart", request.YearsRangeStart);
+            col.AddWithValue("@YearsRangeEnd", request.YearsRangeEnd);
+        }
+
+
+    }
+}
